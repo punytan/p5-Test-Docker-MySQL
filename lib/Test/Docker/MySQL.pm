b80e293f2ef5ab847626b3b4ab698bfa211335a7
punytan <punytan@gmail.com>
2014-07-10 20:58:10 +0900

    typo

--- lib/Test/Docker/MySQL.pm
+++ lib/Test/Docker/MySQL.pm
@@ -129,8 +129,8 @@ Test::Docker::MySQL is a module to launch MySQL in docker containers.
     use Test::Docker::MySQL;
     my $dm_guard = Test::Docker::MySQL->new;
 
-    my $port_1 = $guard->get_port; # get a mysql container port
-    my $port_2 = $guard->get_port; # get another mysql container port
+    my $port_1 = $dm_guard->get_port; # get a mysql container port
+    my $port_2 = $dm_guard->get_port; # get another mysql container port
 
     my $dsn_1 = "dbi:mysql:database=mysql;host=127.0.0.1;port=$port_1";
     my $dbh_1 = DBI->connect($dsn , 'root', '', { RaiseError => 1 });
@@ -158,7 +158,7 @@ The tag to launch via Docker. Default value is C<punytan/p5-test-docker-mysql>.
 
 =item C<ports>
 
-Specify port range by C<ports>>. Default value is C<[ 55500 .. 55555 ]>,
+Specify port range by C<ports>. Default value is C<[ 55500 .. 55555 ]>,
 
 =back
 
